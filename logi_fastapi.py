72377f34fc78270b2e4d315733142ce48f1340d6
niikun <123465566+niikun@users.noreply.github.com>
2024-06-17 14:25:17 +0000

    add FastAPI for logistic

---
+++ logi_fastapi.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+import uvicorn
+from mylib.logistics import distance
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Duke"}
+
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1: int, num2: int):
+    """
+    Add two numbers together
+    """
+    total = num1 + num2
+    return {"total": total}
+
+
+@app.get("/distance/{city1}/{city2}")
+async def get_distance(city1: str, city2: str):
+    """
+    Get the distance between two cities
+    """
+    dist = distance(city1, city2)
+    return {"distance": dist}
+
+
+@app.get("/hours/{city1}/{city2}/{speed}")
+async def get_hours(city1: str, city2: str, speed: int):
+    """
+    Get the hours between two cities
+    """
+    dist = distance(city1, city2)
+    hours = dist / speed
+    return {"hours": hours}
+
+@app.post("/distance")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
